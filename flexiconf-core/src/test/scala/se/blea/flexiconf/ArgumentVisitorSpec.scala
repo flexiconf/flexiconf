37222f2c1db50d1867e34271f46ade4db31a61e3
Tristan Blease <tristan@blea.se>
2015-07-03 10:03:06 -0500

    Allow parsing of signed numbers

--- flexiconf-core/src/test/scala/se/blea/flexiconf/ArgumentVisitorSpec.scala
+++ flexiconf-core/src/test/scala/se/blea/flexiconf/ArgumentVisitorSpec.scala
@@ -13,10 +13,10 @@ class ArgumentVisitorSpec extends FlatSpec with Matchers with ConfigHelpers {
   }
 
   it should "return list of arguments when visiting argument lists" in {
-    val ctx = parse( """1 off "true" 500.2 foo bar 100ms 25%""")
+    val ctx = parse( """1 off "true" 500.2 foo bar 100ms 25% -600""")
     val result = ArgumentVisitor(ctx.argumentList)
 
-    assert(result.size == 8)
+    assert(result.size == 9)
     assert(result.forall(_.isInstanceOf[Argument]))
 
     assert(result(0).kind == IntArgument)
@@ -27,7 +27,7 @@ class ArgumentVisitorSpec extends FlatSpec with Matchers with ConfigHelpers {
     assert(result(5).kind == StringArgument)
     assert(result(6).kind == DurationArgument)
     assert(result(7).kind == PercentageArgument)
-
+    assert(result(8).kind == IntArgument)
   }
 
   it should "return string arguments for quoted string arguments" in {
@@ -78,26 +78,42 @@ class ArgumentVisitorSpec extends FlatSpec with Matchers with ConfigHelpers {
   }
 
   it should "return integer arguments for integer values" in {
-    val result = ArgumentVisitor(parse("10001").argument)
-    assert(result(0).kind == IntArgument)
-    assert(result(0).originalValue == "10001")
+    val result = ArgumentVisitor(parse("10001").argument)(0)
+    (result.kind, result.originalValue, result.value) shouldEqual (IntArgument, "10001", LongValue(10001))
+  }
+
+  it should "return integer arguments for negative integer values" in {
+    val result = ArgumentVisitor(parse("-10001").argument)(0)
+    (result.kind, result.originalValue, result.value) shouldEqual (IntArgument, "-10001", LongValue(-10001))
   }
 
   it should "return decimal arguments for decimal values" in {
-    val result = ArgumentVisitor(parse("0.300").argument)
-    assert(result(0).kind == DecimalArgument)
-    assert(result(0).originalValue == "0.300")
+    val result = ArgumentVisitor(parse("0.300").argument)(0)
+    (result.kind, result.originalValue, result.value) shouldEqual (DecimalArgument, "0.300", DoubleValue(0.3))
+  }
+
+  it should "return decimal arguments for negative decimal values" in {
+    val result = ArgumentVisitor(parse("-0.300").argument)(0)
+    (result.kind, result.originalValue, result.value) shouldEqual (DecimalArgument, "-0.300", DoubleValue(-0.3))
   }
 
   it should "return duration arguments for duration values" in {
-    val result = ArgumentVisitor(parse("10s").argument)
-    assert(result(0).kind == DurationArgument)
-    assert(result(0).originalValue == "10s")
+    val result = ArgumentVisitor(parse("10s").argument)(0)
+    (result.kind, result.originalValue, result.value) shouldEqual (DurationArgument, "10s", LongValue(10*1000))
+  }
+
+  it should "return duration arguments for negative duration values" in {
+    val result = ArgumentVisitor(parse("-10s").argument)(0)
+    (result.kind, result.originalValue, result.value) shouldEqual (DurationArgument, "-10s", LongValue(-1*10*1000))
   }
 
   it should "return percentage arguments for percentage values" in {
-    val result = ArgumentVisitor(parse("10%").argument)
-    assert(result(0).kind == PercentageArgument)
-    assert(result(0).originalValue == "10%")
+    val result = ArgumentVisitor(parse("10%").argument)(0)
+    (result.kind, result.originalValue, result.value) shouldEqual (PercentageArgument, "10%", DoubleValue(0.1))
+  }
+
+  it should "return percentage arguments for negative percentage values" in {
+    val result = ArgumentVisitor(parse("-10%").argument)(0)
+    (result.kind, result.originalValue, result.value) shouldEqual (PercentageArgument, "-10%", DoubleValue(-0.1))
   }
 }
